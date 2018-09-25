7513eb87de126924f3bdf8a8d667248aa2b1ee9e
Seth <sethalexander@pm.me>
2018-09-24 22:02:06 -0500

    :rocket:

---
+++ gatsby-config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        baseUrl: 'sethaalexander.com',
+        protocol: 'https',
+        hostingWPCOM: false,
+        useACF: true,
+        // Include specific ACF Option Pages that have a set post ID
+        // Regardless if an ID is set, the default options route will still be retrieved
+        // Must be using V3 of ACF to REST to include these routes
+        // Example: `["option_page_1", "option_page_2"]` will include the proper ACF option
+        // routes with the ID option_page_1 and option_page_2
+        // Dashes in IDs will be converted to underscores for use in GraphQL
+        acfOptionPageIds: [],
+        auth: {
+          // If auth.user and auth.pass are filled, then the source plugin will be allowed
+          // to access endpoints that are protected with .htaccess.
+          htaccess_user: '',
+          htaccess_pass: '',
+          htaccess_sendImmediately: false,
+        },
+        verboseOutput: true,
+        perPage: 100,
+        concurrentRequests: 10,
+        // Exclude specific routes using glob parameters
+        // See: https://github.com/isaacs/minimatch
+        // Example:  `["/*/*/comments", "/yoast/**"]` will exclude routes ending in `comments` and
+        // all routes that begin with `yoast` from fetch.
+        excludedRoutes: ['/*/*/comments', '/yoast/**'],
+      },
+    },
+  ],
+}
